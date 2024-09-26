572dc4eb8f199cf10000efeb3f4c926a84d667a3
arimmu <1221404256@student.mmu.edu.my>
2024-09-26 19:08:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ with col1:
     y = df['signal']  # Target variable
 
     # Train-Test-Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=10)
 
     # Model Creation: K-NN Classifier
     knn = KNeighborsClassifier(n_neighbors=3)
