6fae15e0d9191020e4b2f0a6035a46a9c72f087e
arimmu <1221404256@student.mmu.edu.my>
2024-09-26 21:54:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ with col1:
     
     X = df[selected_features].drop("Close", axis=1)  # Features
     y = df['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=40)
     
     # K-NN Regressor for Prediction
     knn_reg = KNeighborsRegressor()
