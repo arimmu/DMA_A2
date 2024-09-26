59415fd5a250595ba3032bc2f60ebe5e33558679
arimmu <1221404256@student.mmu.edu.my>
2024-09-27 07:36:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ with col1:
     
     ticker = st.text_input("Enter Stock Ticker", value="1155.KL")
     # Date inputs from user
-    startDate = st.date_input("Start Date", value=pd.to_datetime("2024-08-01"))
-    endDate = st.date_input("End Date", value=pd.to_datetime("2024-09-24")) + timedelta(days=1)
+    startDate = st.date_input("Start Date", value=pd.to_datetime("2024-08-05"))
+    endDate = st.date_input("End Date", value=pd.to_datetime("2024-09-23")) + timedelta(days=1)
     tf = "1d"  # Interval
 
     # Load data from Yahoo Finance
