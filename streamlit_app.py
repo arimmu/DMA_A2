cdd01f8a376a5e0ff89f1dababd4ad7584cf0c52
arimmu <1221404256@student.mmu.edu.my>
2024-09-26 19:14:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ with col1:
     st.write(f'AUC: {auc_knn:.2f}')
 
     X = df[selected_features].drop("Close", axis=1)  # Features
-    y = df['close']
+    y = df['Close']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=42)
     
     # K-NN Regressor for Prediction
