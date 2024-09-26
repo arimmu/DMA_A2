9ef036d46e0bdd612a89d8d58af354773b4f3c4e
arimmu <1221404256@student.mmu.edu.my>
2024-09-27 06:15:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ with col1:
     y = df['signal']  # Target variable
 
     # Train-Test-Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=10)
 
     # Model Creation: K-NN Classifier
     knn = KNeighborsClassifier(n_neighbors=3)
@@ -128,7 +128,7 @@ with col1:
     
     X = df[selected_features].drop("Close", axis=1)  # Features
     y = df['Close']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=10)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state=10)
     
     # K-NN Regressor for Prediction
     knn_reg = KNeighborsRegressor()
